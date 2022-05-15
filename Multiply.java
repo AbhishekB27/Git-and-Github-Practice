5a9dd38c23ad98f0ce8637a3f252233255c7b59c
AbhishekB27 <akstyles27@gmail.com>
2022-05-15 15:56:45 +0530

    add multiply functionality

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static int mul(int a , int b) {
+        int result = a * b;
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(Multiply.mul(5, 9));
+    }
+}
