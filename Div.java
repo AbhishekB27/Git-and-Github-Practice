c3dd0e439063f911afe53f2eeaf6a3b584adcd5f
AbhishekB27 <akstyles27@gmail.com>
2022-05-15 16:27:36 +0530

    add division functionality

--- Div.java
+++ Div.java
@@ -1,5 +1,10 @@
 public class Div {
+    public static int divide(int a , int b) {
+        int result;
+        result = a / b;
+        return result;
+    }
     public static void main(String[] args) {
-        
+        System.out.println(Div.divide(200, 5));
     }
 }
