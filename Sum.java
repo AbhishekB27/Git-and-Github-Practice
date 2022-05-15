b41f9a8825277dbd0f14726e5b0f84ac212b11d1
AbhishekB27 <akstyles27@gmail.com>
2022-05-15 15:26:41 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+class Sum{
+    public static int add(int a, int b) {
+        int c;
+        c=a+b;
+        return c;
+    }
+    public static void main(String args[]) {
+        System.out.println(Sum.add(5,9));
+    }
+}
