f61044e6b7cec029c240bce0b51f7b959d4fb085
AbhishekB27 <akstyles27@gmail.com>
2022-05-15 15:33:36 +0530

    adding three No. functionality

--- Sum.java
+++ Sum.java
@@ -1,10 +1,10 @@
 class Sum{
-    public static int add(int a, int b) {
-        int c;
-        c=a+b;
-        return c;
+    public static int add(int a, int b, int c) {
+        int total;
+        total=a+b;
+        return total;
     }
     public static void main(String args[]) {
-        System.out.println(Sum.add(5,9));
+        System.out.println("Sum of Three No. is: " + Sum.add(5,9,6));
     }
 }
